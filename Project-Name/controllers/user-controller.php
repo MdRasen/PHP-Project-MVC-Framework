c159e437a0b25375c04975236a16e77cade0aae2
Md Al Amin Hossen <aamin.hossen99@gmail.com>
2023-11-29 16:06:20 +0600

    Update user-controller.php

--- Project-Name/controllers/user-controller.php
+++ Project-Name/controllers/user-controller.php
@@ -1,5 +1,6 @@
 <?php
-// Database connection with controller & user-model
+// Database connection with controller, model & custom-models
 require 'config/dbcon.php';
 require 'controller.php';
+require '../models/model.php';
 require '../models/user-model.php';
